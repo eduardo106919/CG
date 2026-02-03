004b13099da329939ba952765a3569839cfda571
eduardo106919 <a106919@alunos.uminho.pt>
2026-02-03 12:25:01 +0000

    added worksheet 01 resolution

--- fichas/ficha-01/main.cpp
+++ fichas/ficha-01/main.cpp
@@ -8,10 +8,12 @@
 #include <math.h>
 #include <stdio.h>
 
+double size = 0;
+
 void changeSize(int w, int h)
 {
 	// Prevent a divide by zero, when window is too short
-	// (you can�t make a window with zero width).
+	// (you can't make a window with zero width).
 	if (h == 0)
 		h = 1;
 	// compute window's aspect ratio
@@ -29,49 +31,69 @@ void changeSize(int w, int h)
 	glViewport(0, 0, w, h);
 }
 
-
 void renderScene(void)
 {
 	// clear buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 	// set camera
 	glLoadIdentity();
 	gluLookAt(0.0f, 0.0f, 5.0f,
-		0.0f, 0.0f, -1.0f,
-		0.0f, 1.0f, 0.0f);
-		
-	// put drawing instructions here
+			  0.0f, 0.0f, -1.0f,
+			  0.0f, 1.0f, 0.0f);
 
-	
+	// custom camera
+	// gluLookAt(5.0f, 7.0f, 10.0f,
+	// 		  0.0f, 0.0f, -1.0f,
+	// 		  0.0f, 1.0f, 0.0f);
+
+	// put drawing instructions here
+	// glutWireCube(2);
+	// glutWireSphere(1.5, 20, 10);
+	// glutWireCone(2, 5, 15, 5);
+	// glutWireTorus(0.5, 4, 10, 15);
+	// glutWireIcosahedron();
+	// glutWireDodecahedron();
+	// glutWireOctahedron();
+	// glutWireTetrahedron();
+
+	glutWireTeapot(sin(size) * 0.5 + 1);
+	size += 0.01;
+
 	// End of frame
 	glutSwapBuffers();
 }
 
-
-void printInfo() {
-
+void printInfo()
+{
 	printf("Vendor: %s\n", glGetString(GL_VENDOR));
 	printf("Renderer: %s\n", glGetString(GL_RENDERER));
 	printf("Version: %s\n", glGetString(GL_VERSION));
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	// put GLUT init here
-	
-    
-	// put callback registry here
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("CG@DI");
 
-	
+	// put callback registry here
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+
+	// no events, does this function
+	glutIdleFunc(renderScene);
+
 	// some OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	
-	// enter GLUT�s main cycle
+
+	// enter GLUT's main cycle
 	glutMainLoop();
-	
+
 	return 1;
 }
